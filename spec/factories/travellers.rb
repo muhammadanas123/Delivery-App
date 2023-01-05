d5061baa11fd50874055ffdb215328f2c4220739
muhammadanas123 <anasfarooqi53@gmail.com>
2023-01-05 19:55:05 +0500

    setup rspecs and factory bot and test traveller

---
+++ spec/factories/travellers.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+
+  factory :traveller do
+    firstname { "anas" }
+    lastname { "farooqi" }
+    phone_no { 03332221111 }
+    landline { 03331112222 }
+    city { "lahore" }
+    state { "punjab" }
+    country { "pakistan" }
+  end
+
+end
