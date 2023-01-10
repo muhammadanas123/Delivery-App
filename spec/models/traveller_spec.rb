6c34a62bdfc989b833ea66d4da30940208c99ec9
muhammadanas123 <anasfarooqi53@gmail.com>
2023-01-10 13:50:52 +0500

    removed byebug

--- spec/models/traveller_spec.rb
+++ spec/models/traveller_spec.rb
@@ -4,10 +4,7 @@ RSpec.describe Traveller, type: :model do
   context "when creating a traveller" do
     let(:traveller) { build :traveller }
     it "should be valid traveller with all attributes" do
-      byebug
       expect(traveller.valid?).to eq(true)
     end
-
   end
-  
 end
